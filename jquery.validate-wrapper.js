a56ac9faac55813e218a6ac1849e315840aeae5f
Siddhant <naiksiddhant335@gmail.com>
2019-06-24 22:39:47 +0530

    update the plugin with group validations and required_from_group validations

--- jquery.validate-wrapper.js
+++ jquery.validate-wrapper.js
@@ -4,7 +4,7 @@
 	Basically, the wrapper plugin will help you to validate form elements by adding a few lines of code. Just use the form selector and call the wrapper plugin and the form will start validating. This will help the developers to maintain a single file for all the form. It will also help the developers in terms of code redundancy, latency, and many other aspects.
 */
 ; (function ($, window, document, undefined) {
-	
+
 	"use strict";
 
 	//Default Parameters
@@ -17,13 +17,18 @@
 		this._pluginName = pluginName;
 		this._defaults   = $.fn[pluginName].defaults;
 		this._settings   = $.extend({}, this._defaults, options);
-		delete this._settings.validatorMessages;  //removing validator messages from _settings
-		delete this._settings.validateGroup; // removing defined validate group from _settings
-		if (options.validatorMessages && typeof options.validatorMessages != "object") delete options.validatorMessages;
-		if (options.validateGroup && typeof options.validateGroup != "object") delete options.validateGroup;
-		//extending validators messages
-		this._validatorMessages = $.extend({}, this._defaults.validatorMessages, options.validatorMessages);
-		this._validateGroup     = $.extend({}, this._defaults.validateGroup, options.validateGroup);
+
+		delete this._settings.messages;  //removing validator messages from _settings
+		delete this._settings.groups; // removing defined validate group from _settings
+		delete this._settings.require_from_group; // removing defined required_from_group from _settings
+		if (options.messages && typeof options.messages != "object")
+			delete options.messages;
+
+		if (options.groups && typeof options.groups != "object") delete options.groups;
+
+		this._messages     = $.extend({}, this._defaults.messages, options.messages);
+		this._groups         = $.extend({}, this._defaults.groups, options.groups);
+		this._require_from_group = $.extend({}, this._defaults.require_from_group, options.require_from_group);
 
 		this._init();
 	}
@@ -51,39 +56,47 @@
 
 		//validate wrapper
 		_validate: function () {
-			//jquery.validate Function
-			this.$_element.validate({
-				debug              : this._settings.debug,
-				ignore             : this._settings.ignore,
-				onsubmit           : this._settings.onSubmit,
-				focusInvalid       : this._settings.focusInvalid,
-				errorClass         : this._settings.errorClass,
-				errorElement       : this._settings.errorElement,
-				validClass         : this._settings.validClass,
-				errorContainer     : this._settings.errorContainer,
-				errorLabelContainer: this._settings.errorLabelContainer,
-				wrapper            : this._settings.wrapper,
-				showErrors         : null,
-				highlight          : ($.isFunction(this._settings.highlight)) ? this._settings.highlight          : this._highlight,
-				unhighlight        : ($.isFunction(this._settings.unhighlight)) ? this._settings.unhighlight      : this._unHighlight,
-				invalidHandler     : ($.isFunction(this._settings.invalidHandler)) ? this._settings.invalidHandler: this._inValidHandler,
-				groups             : (typeof this._validateGroup === "object") ? this._validateGroup              : null,
-				errorPlacement     : ($.isFunction(this._settings.errorPlacement)) ? this._settings.errorPlacement: this._errorPlacement,
-				success            : this._settings.success,
-				submitHandler      : function (form) {
+			let extraParams = {
+				highlight     : ($.isFunction(this._settings.highlight)) ? this._settings.highlight          : this._highlight,
+				unhighlight   : ($.isFunction(this._settings.unhighlight)) ? this._settings.unhighlight      : this._unHighlight,
+				invalidHandler: ($.isFunction(this._settings.invalidHandler)) ? this._settings.invalidHandler: this._inValidHandler,
+				groups        : (typeof this._groups === "object") ? this._groups              : null,
+				errorPlacement: ($.isFunction(this._settings.errorPlacement)) ? this._settings.errorPlacement: this._errorPlacement,
+				submitHandler : function (form) {
 					plugin._callback(form);
 				},
-			});
+			};
+			let validateObj = $.extend({}, this._settings, extraParams);
+			//jquery.validate Function
+			this.$_element.validate(validateObj);
 		},
 
 		//additional functions of jquery validate listed here.
 		_additionalMethod: function () {
 			//Default validator messages
-			jQuery.extend(jQuery.validator.messages, this._validatorMessages);
+			jQuery.extend(jQuery.validator.messages, this._messages);
+
+			//Rule to set require_from_group validation
+			for (var key in this._require_from_group) {
+				jQuery.validator.addClassRules(key, {
+					'require_from_group': [this._require_from_group[key], '.' + key]
+				});
+			}
+
 			//Rule to set group validation
-			jQuery.validator.addClassRules('group-all-together', {
-				'require_from_group': [jQuery('.group-all-together').length, '.group-all-together']
-			});
+			for (var i in this._groups) {
+				for (var k in this._messages) {
+					if (i == k) {
+						if (this._messages[k] != '') {
+							$('.' + i).each(function () {
+								$("#" + this.id).rules("add", {
+									messages: { required: plugin._messages[k] }
+								});
+							});
+						}
+					}
+				}
+			}
 		},
 
 		//Jquery Validator Function default definition
@@ -113,15 +126,20 @@
 				error.insertAfter(element.parent());
 			else if (element.is('input[type=checkbox]') || element.is('input[type=radio]'))
 				element.closest('.form-check').parent().append(error);
-			else if (element.hasClass('group-all-together')) {
-				var lastElement = $('.group-all-together:last');
-				if ($(element).attr('name') == $(lastElement).attr('name'))
-					error.insertAfter(lastElement.parent().parent());
+			else if (element.hasClass('required-from-group')) {
+				for (var k in plugin._require_from_group) {
+					var lastElement = $('.' + k + ':last');
+					if ($(element).attr('name') == $(lastElement).attr('name')) {
+						error.insertAfter(lastElement.parent().parent());
+						break;
+					}
+				}
 			} else if (element.hasClass('group-in-one')) {
-				for (var key in plugin._validateGroup) {
+				for (var key in plugin._groups) {
 					if (element.hasClass(key)) {
 						var lastEl = $('.' + key + ':last');
 						error.insertAfter(lastEl.parent().parent());
+						break;
 					}
 				}
 			} else
@@ -150,25 +168,18 @@
 
 	//setting Default values
 	$.fn[pluginName].defaults = {
-		debug              : false,
-		ignore             : ":hidden:not(.hidden-required), .ignore",
-		onsubmit           : null,
-		focusInvalid       : false,
-		errorClass         : 'error',
-		errorElement       : 'div',
-		validClass         : 'success',
-		errorContainer     : null,
-		errorLabelContainer: null,
-		wrapper            : null,
-		showErrors         : null,
-		highlight          : null,
-		unhighlight        : null,
-		invalidHandler     : null,
-		errorPlacement     : null,
-		success            : null,
-		onComplete         : null,                                       //callback function called in submitHandler
-		validateGroup      : null,
-		validatorMessages  : { //JQuery validator default messages
+		ignore            : ":hidden:not(.hidden-required), .ignore",
+		errorClass        : 'error',
+		errorElement      : 'div',
+		validClass        : 'success',
+		highlight         : null,
+		unhighlight       : null,
+		invalidHandler    : null,
+		errorPlacement    : null,
+		onComplete        : null,
+		groups            : null,
+		require_from_group: null,
+		messages          : { //JQuery validator default messages
 			require_from_group: jQuery.validator.format("Please fill out all {0} fields.")
 		}
 	};
